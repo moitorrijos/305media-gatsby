bfd941bc9b564adc2e58aedc10ecc62c5465406d
moitorrijos <juanmtorrijos@gmail.com>
2020-06-25 13:34:20 -0500

    Remove play button in index page and video modal.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react"
-import PlayButton from "../icons/play-button.svg"
+import React /* , { useState } */ from "react"
+// import PlayButton from "../icons/play-button.svg"
 import MediaButton from "./media-button"
-import VideoModal from "./video-modal"
+// import VideoModal from "./video-modal"
 import DownArrow from "../icons/down-arrow.svg"
 
 const Hero = () => {
-  const [show, setShow] = useState(false)
+  // const [show, setShow] = useState(false)
   return (
     <>
-      <VideoModal
+      {/* <VideoModal
         src="https://www.youtube.com/embed/sRFdqk4iDV8"
         title="Gente que Habla Como Tú"
         show={show}
         setShow={setShow}
-      />
+      /> */}
       <div className="hero">
         <h1>
           Gente que habla como <br /> <strong>tú</strong>
         </h1>
         <MediaButton />
-        <button
+        {/* <button
           className="play-button"
           onClick={() => {
             setShow(!show)
           }}
         >
           <PlayButton />
-        </button>
+        </button> */}
         <DownArrow className="down-arrow" />
       </div>
     </>
