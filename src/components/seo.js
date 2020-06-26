2528f0c9990347a3bb4b101a2f5bff26b5c2306a
moitorrijos <juanmtorrijos@gmail.com>
2020-06-26 15:49:14 -0500

    Improve ogimage.

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import ogImage from "../images/website-icon.png"
+import ogImage from "../images/website-icon-ogimage.png"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
